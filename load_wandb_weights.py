2b92344fae39b09993720da5c76f1fb8fe6f7ede
Nikita Balagansky <balaganskij.nn@phystech.edu>
2024-03-09 16:01:40 +0300

    fix

--- load_wandb_weights.py
+++
@@ -1,9 +0,0 @@
-import wandb
-import pyrallis
-
-api = wandb.Api(api_key="471ddfc926443ae433952cc41e166ad64cd303bf")
-rebased_run_name = "harmless/zoology_pile/4501437"
-run = api.run(rebased_run_name)
-with open("config.yaml", "w") as f:
-    pyrallis.dump(run.config['model_config'], f)
-            
